797f0b4ad0e035ac1f944c610d875b35a0594e58
_Kerman <kermanx@qq.com>
2025-05-10 12:41:49 +0800

    feat: basic a2a server

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import uvicorn
+from dotenv import load_dotenv
+from a2a_server import start_a2a
 
 def main():
+    print("Starting A2A server...")
+    start_a2a(host="localhost", port=8001)
     print("Starting FastAPI server...")
     uvicorn.run("server:app", host="localhost", port=8000, reload=True)
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
---
+++ third/README.md
@@ -0,0 +1,3 @@
+# Third Party Libraries
+
+- [a2a-python-sdk](./a2a-python-sdk/): Copied from https://github.com/google/A2A/tree/main/a2a-python-sdk.
