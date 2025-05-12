6b65aec0c7677aa5b38dc5677c7ab482d793aca0
_Kerman <kermanx@qq.com>
2025-05-12 16:56:03 +0800

    chore: move server to subdir

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
     load_dotenv()
     subprocess.Popen([sys.executable, "./a2a_server/main.py"], shell=True)
     print("Starting FastAPI server...")
-    uvicorn.run("server:app", host="localhost", port=8000, reload=True)
+    uvicorn.run(
+        "server.server:app",
+        ost="localhost",
+        port=8000,
+        reload=True,
+    )
