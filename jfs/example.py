3a181ef478e466de2b4bc57dee88fea449241a5c
HIJII_ZHANG <zhanghaichuan@mail.ustc.edu.cn>
2025-05-28 05:56:30 +0000

    chore: lint and format code

--- jfs/example.py
+++ jfs/example.py
@@ -1,11 +1,8 @@
-from juicefs import Client
 import juicefs
 # 创建 JuiceFS 客户端
-#jfs = Client(name="", meta="sqlite3://./jfs/jfs.db")
+# jfs = Client(name="", meta="sqlite3://./jfs/jfs.db")
 
 # 列出目录中的文件
-#jfs.listdir("/")
+# jfs.listdir("/")
 
 help(juicefs.Client)
-
-
