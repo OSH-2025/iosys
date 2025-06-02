6959684a278ea128b0e939524a97ba6800af84f3
HIJII_ZHANG <zhanghaichuan@mail.ustc.edu.cn>
2025-06-02 15:46:09 +0000

    chore: lint and format code

--- jfs/example.py
+++ jfs/example.py
@@ -1,8 +1,14 @@
-import juicefs, os
-#print("JuiceFS version:", juicefs.__version__)
-print("Shared lib :", os.path.exists(os.path.join(os.path.dirname(juicefs.__file__),"libjfs.so")))
+import juicefs
+import os
+
+# print("JuiceFS version:", juicefs.__version__)
+print(
+    "Shared lib :",
+    os.path.exists(os.path.join(os.path.dirname(juicefs.__file__), "libjfs.so")),
+)
 from juicefs import Client
+
 print("Client OK →", Client(name="myjfs", meta="sqlite3://./jfs/jfs.db").listdir("/"))
 
 
-#help(juicefs.Client)
+# help(juicefs.Client)
