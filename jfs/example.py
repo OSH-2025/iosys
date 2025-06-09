abb7280e3f7db2d433690a29c12fcaafdee0ffe6
HIJII_ZHANG <zhanghaichuan@mail.ustc.edu.cn>
2025-06-10 00:48:35 +0800

    fix:rm token

--- jfs/example.py
+++ jfs/example.py
@@ -1,14 +1,24 @@
+import os
+import juicefs_local
 # print("JuiceFS version:", juicefs.__version__)
-"""
 print(
     "Shared lib :",
     os.path.exists(os.path.join(os.path.dirname(juicefs_local.__file__), "libjfs.so")),
 )
 from juicefs_local import Client
 
-print("Client OK →", Client(name="iosys", meta="sqlite3://./data/jfs.db").listdir("/"))
-"""
+jfs=Client(name="iosys", meta="sqlite3://./data/jfs.db")
 
+jfs.makedirs("/files")
+with jfs.open("/files/hello.txt", "w") as f:
+    f.write("hello")
+with jfs.open("/files/hello.txt", "a+") as f:
+    f.write(" world")
+with jfs.open("/files/hello.txt") as f:
+    data = f.read()
+    print(data)
+
+'''
 import juicefs
 
 jfs = juicefs.Client(
@@ -17,3 +27,4 @@ jfs = juicefs.Client(
 
 print(jfs.listdir("/"))
 # help(juicefs.Client)
+'''
