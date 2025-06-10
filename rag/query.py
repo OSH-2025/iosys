b1d8d69e6c6ca9d58edc13f0f0f6f4f5b1a3b881
_Kerman <kermanx@qq.com>
2025-06-10 11:44:41 +0800

    fix(rag): use sync qdrant client

--- rag/query.py
+++ rag/query.py
@@ -1,7 +1,6 @@
 import os
 from typing import cast
-from qdrant_client import AsyncQdrantClient
-from qdrant_client.models import VectorParams, Distance
+from qdrant_client import QdrantClient
 import asyncio
 
 from llama_index.core import VectorStoreIndex, StorageContext
@@ -30,9 +29,9 @@ class IOSYSQueryEngine:
         )
         qdrant_path = os.environ["QDRANT_DATABASE_PATH"]
         qdrant_exists = os.path.exists(qdrant_path)
-        qdrant_client = AsyncQdrantClient(path=qdrant_path)
+        qdrant_client = QdrantClient(path=qdrant_path)
         vector_store = QdrantVectorStore(
-            aclient=qdrant_client,
+            client=qdrant_client,
             collection_name="iosys",
         )
         if qdrant_exists:
@@ -44,12 +43,6 @@ class IOSYSQueryEngine:
             )
         else:
             print("[VectorIndex]: Collection 'iosys' does not exist, creating it.")
-            self.init_task = asyncio.create_task(
-                qdrant_client.create_collection(
-                    collection_name="iosys",
-                    vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
-                )
-            )
             self.index = VectorStoreIndex(
                 embed_model=self.embed_model,
                 storage_context=StorageContext.from_defaults(
@@ -67,7 +60,7 @@ class IOSYSQueryEngine:
     async def create_node(self, path: str, parsed: IOSYSParsedFile):
         await self._ensure_initialized()
         print(f"[VectorIndex]: Creating node for {path}")
-        await self.index.ainsert(
+        self.index.insert(
             Document(
                 doc_id=path,
                 text=parsed.brief_text,
@@ -77,7 +70,7 @@ class IOSYSQueryEngine:
     async def update_node(self, path: str, parsed: IOSYSParsedFile):
         await self._ensure_initialized()
         print(f"[VectorIndex]: Updating node for {path}")
-        await self.index.arefresh_ref_docs(
+        self.index.refresh_ref_docs(
             [
                 Document(
                     doc_id=path,
@@ -88,11 +81,11 @@ class IOSYSQueryEngine:
 
     async def delete_node(self, path: str):
         await self._ensure_initialized()
-        await self.index.adelete_nodes([path])
+        self.index.delete_nodes([path])
 
     async def query_nodes(self, query: str):
         await self._ensure_initialized()
         engine = self.index.as_query_engine(llm=self.llm, use_async=True)
         print(f"[VectorIndex]: Querying nodes with query: {query}")
-        result = await engine.aquery(query)
+        result = engine.query(query)
         return cast(Response, result)
